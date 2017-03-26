7646022ba8a4b3a50e45002690a4efb36f0a5622
bytetonight <isisteam@web.de>
2017-03-26 18:02:24 +0200

    -Implemented Parcelable to make ScoreStorage storable in orientation changes

--- app/src/main/java/android/example/com/courtcounter/ScoreStorage.java
+++ app/src/main/java/android/example/com/courtcounter/ScoreStorage.java
@@ -1,10 +1,13 @@
 package android.example.com.courtcounter;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * A simple Dataholder-Class offering methods to add and retrieve data from Main Activity
  * for the sake of splitting up logic
  */
-public class ScoreStorage {
+public class ScoreStorage implements Parcelable{
 
     private String id = null;
     private int triples = 0;
@@ -12,6 +15,10 @@ public class ScoreStorage {
     private int singles = 0;
     private int fouls = 0;
 
+    public ScoreStorage(Parcel in)
+    {
+
+    }
 
     public ScoreStorage(String id)
     {
@@ -26,6 +33,8 @@ public class ScoreStorage {
         fouls = 0;
     }
 
+
+//region Getters & Setters
     public int getScore() {
         return (triples * 3) + (doubles * 2) + singles;
     }
@@ -69,6 +78,51 @@ public class ScoreStorage {
     public int getSingles() {
         return singles;
     }
+//endregion
 
+//region parcelable implementation
+
+    public static final Creator<ScoreStorage> CREATOR = new Creator<ScoreStorage>()
+    {
+        @Override
+        public ScoreStorage createFromParcel(Parcel in)
+        {
+            return new ScoreStorage(in);
+        }
+
+        @Override
+        public ScoreStorage[] newArray(int size)
+        {
+            return new ScoreStorage[size];
+        }
+    };
+
+    /**
+     * Describe the kinds of special objects contained in this Parcelable's
+     * marshalled representation.
+     *
+     * @return a bitmask indicating the set of special object types marshalled
+     * by the Parcelable.
+     */
+    @Override
+    public int describeContents()
+    {
+        return 0;
+    }
+
+    /**
+     * Flatten this object in to a Parcel.
+     *
+     * @param dest  The Parcel in which the object should be written.
+     * @param flags Additional flags about how the object should be written.
+     *              May be 0 or {@link #PARCELABLE_WRITE_RETURN_VALUE}.
+     */
+    @Override
+    public void writeToParcel(Parcel dest, int flags)
+    {
+
+    }
+
+//endregion
 
 }
